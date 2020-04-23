08dd218b988fede14eb23065e4e97c98e6fa78a8
Jandrei Carlos Masiero <jandrei@ibm.com>
2020-04-23 20:06:14 -0300

    mensage

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/mars2', function (req, res) {
+  res.send('Hello World mars 2!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
